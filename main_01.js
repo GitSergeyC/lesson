f3f51954df742c990af31b417795f5109ec65510
GitSergeyC <sergey.chumakov.work@gmail.com>
2024-03-28 19:27:42 +0500

    4

---
+++ main_01.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    // const sladers = document.querySelector(".sladers");
+   
+    const miniatures = document.querySelector(".miniatures");
+    const miniature = document.querySelector(".miniature");
+
+    const arowleft = document.querySelector(".arow-left");
+    const arowright = document.querySelector(".arow-right");
+
+		
+
+	
+
+		let sladerIndex = 0;
+		const slader = document.querySelectorAll(".slader");
+		
+		const content = document.querySelector(".contents").addEventListener("click", event => {
+				if (event.target.classList.contains("arow-right")) {
+						slader[sladerIndex].classList.remove("activ");
+						sladerIndex = sladerIndex >= 2 ? 0 : sladerIndex + 1
+						slader[sladerIndex].classList.add("activ");
+						
+				} 
+				
+
+				if (event.target.classList.contains("arow-left")) {
+					slader[sladerIndex].classList.remove("activ");
+					sladerIndex = sladerIndex <= 0 ? 2 : sladerIndex - 1;
+					slader[sladerIndex].classList.add("activ");
+			} 
+		});
+});	
+
